049df0c7d3e16a583de0ffe47f5bd133730595dc
jihanzhuang <869794967@qq.com>
2016-07-31 06:33:29 -0700

    splitwords

--- tests/pinyin.php
+++ tests/pinyin.php
@@ -2,5 +2,14 @@
 //
 $a=new Pinyin();
 //echo $a->format("üé  ǜ  à");
-echo $a->splitWords("üé  ǜ  à","ascii");
+print_r($a->splitWords("üé  ǜ  à","ascii"));
+
+$option="ascii";
+$pinyin="üé  ǜ  à";
+$split = array_filter(preg_split('/[^üāēīōūǖáéíóúǘǎěǐǒǔǚàèìòùǜa-z\d]+/iu', $pinyin));
+	foreach ($split as $index => $pinyin) {
+
+		$split[$index] = $a->format($pinyin, true);
+		            }
+print_r(array_values($split));
 
